ecab70a6903fc34dfdf52e28575e64db0cf13122
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-06 17:04:30 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-		programmer_hash[grace_hopper:]
+		programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-		programmer_hash[alan_kay:][known_for:]
+		programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-		programmer_hash[dennis_ritchie:][languages:][0]
+		programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -69,10 +69,6 @@ def adding_matz
 
 	programmer_hash =
  		{
-			:yukihiro_matsumoto => {
-				:known_for => "Ruby",
-				:languages => ["LISP", "C"]
-			}
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -86,7 +82,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-		programmer_hash[yukihiro_matsumoto:] = {known_for: "Ruby", languages: ["LISP, "C"]}
+
+		programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
 		programmer_hash
 
 end
@@ -110,6 +107,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
+		alans_new_info = "GUI"
+		programmer_hash[:alan_kay][:known_for] = alans_new_info
+		programmer_hash
 
 end
 
@@ -132,5 +132,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+		programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+		programmer_hash
 
 end
